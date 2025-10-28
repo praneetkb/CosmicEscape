71da722159a316821cc9235eb3e886377c52cc92
Michael Gonzales <mga150@sfu.ca>
2025-10-28 15:32:56 -0700

    added src with project structure

---
+++ src/main/java/ca/sfu/cmpt276/fall2025/team14/app/Program.java
@@ -0,0 +1,11 @@
+package ca.sfu.cmpt276.fall2025.team14.app;
+
+import de.gurkenlabs.litiengine.Game;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Game.init(args);
+        Game.start();
+    }
+}
